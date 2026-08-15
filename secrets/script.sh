ae44ec9615b526e768b4c01faf771cd242aa274a
Nada Hilal <nada.hilal2312@gmail.com>
2026-08-15 18:51:27 +0100

    ADD : added the maria db requeriment script and config and dockerfile

---
+++ secrets/script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+service mariadb start
+
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+mysql -u root <<EOF
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+service mariadb stop
+
+exec mysqld \
+    --bind-address="${DB_HOST}" \
+    --port=3306
