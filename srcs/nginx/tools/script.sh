d317565591e928886c6809a689264e523357f7ca
Nada Hilal <nada.hilal2312@gmail.com>
2026-08-17 11:56:58 +0100

    Update : updating files in nginx (config , script .env)

--- srcs/nginx/tools/script.sh
+++ srcs/nginx/tools/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Check if the SSL certificate already exists
-if [ ! -f "$CERTS_" ]; then
+if [ ! -f "$CERTS" ]; then
 
     # Generate the SSL certificate and private key
     openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
         -keyout "$P_KEY" \
-        -out "$CERTS_" \
+        -out "$CERTS" \
         -subj "/CN=$DOMAIN_NAME" \
         2>/dev/null
 
@@ -21,7 +21,7 @@ fi
 # Replace the placeholders in the NGINX configuration
 sed -i \
     -e "s+DOMAIN_NAME+$DOMAIN_NAME+" \
-    -e "s+CERTS_+$CERTS_+" \
+    -e "s+CERTS+$CERTS+" \
     -e "s+P_KEY+$P_KEY+" \
     /etc/nginx/sites-available/default
 
