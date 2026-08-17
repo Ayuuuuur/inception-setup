e3f924be7080866b445df5aa0d57c70804ac0281
Nada Hilal <nada.hilal2312@gmail.com>
2026-08-17 14:30:39 +0100

    ADD : add files for wordpress

---
+++ srcs/wordpress/tools/script.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+sleep 10
+
+cd /var/www/html/wordpress
+
+wp core download --allow-root
+
+wp core config \
+    --dbname="${DB_NAME}" \
+    --dbuser="${DB_USER}" \
+    --dbpass="${DB_USER_PASS}" \
+    --dbhost="${DB_HOST}" \
+    --allow-root
+
+wp core install \
+    --allow-root \
+    --url="${DOMAIN_NAME}" \
+    --title="${WP_WEB_TITLE}" \
+    --admin_user="${WP_ADMIN_USER}" \
+    --admin_password="${WP_ADMIN_PASSWORD}" \
+    --admin_email="${WP_ADMIN_EMAIL}"
+
+wp user create \
+    "${WP_USER}" \
+    "${WP_USER_EMAIL}" \
+    --user_pass="${WP_USER_PASSWORD}" \
+    --allow-root
+
+chown -R www-data:www-data /var/www/html/wordpress
+
+sed -i \
+    's#listen = /run/php/php7.4-fpm.sock#listen = 0.0.0.0:9000#g' \
+    /etc/php/7.4/fpm/pool.d/www.conf
+
+sed -i \
+    's#chdir = /var/www#chdir = /var/www/html/wordpress#g' \
+    /etc/php/7.4/fpm/pool.d/www.conf
+
+php-fpm7.4 -F
